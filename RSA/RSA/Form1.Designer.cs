a16c3f9ac2bec60527d7ee6f36c08491044530dd
Carlos Gonzalez <contactos503@gmail.com>
2018-04-28 10:39:25 -0600

    RSA AlGORITMO
    C#

---
+++ RSA/RSA/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace RSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(136, 118);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 48);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Escribe el Mensaje";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(143, 60);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 48);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(136, 22);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(136, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mensaje Cifrado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mensaje decifrado";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Cifrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 192);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(249, 114);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(91, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Descifrar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(316, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(320, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(321, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "label6";
+            this.label6.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 327);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "RSA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ RSA/RSA/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        int n, t,j,i;
+        int[] e = new int[100];
+        int[] d = new int[100];
+        int[] temp = new int[100];
+        int[] m = new int[100];
+        int[]en = new int[100];
+        char[] msg ;
+        int p, q;
+
+
+        // numero primos
+        public static bool ePrimo(int num)
+        {
+            int div = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    div++;
+                }
+            }
+
+            return div == 2; 
+        }
+
+
+        public int cd(int x)
+        {
+             int k = 1;
+            while (true)
+            {
+                k = k + t;
+                if (k % x == 0)
+                    return (k / x);
+            }
+            
+        }
+
+
+        public void ce()
+        {
+            int k;
+            k = 0;
+            for ( i = 2; i < t; i++)
+            {
+                if (t % i == 0)
+                    continue;
+
+                    int flag = Convert.ToInt32(ePrimo(i));
+                    if (flag == 1 && i != p && i != q)
+                    {
+                        e[k] = i;
+                        flag = cd(e[k]);
+                        if (flag > 0)
+                        {
+                            d[k] = flag;
+                            k++;
+                        }
+                    if (k == 99)
+                        break;
+
+                    }
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            decrypt();
+        }
+
+        public void encriptar()
+        {
+             int pt, ct, key = e[0], k, len;
+            i = 0;
+            len = textBox3.TextLength;
+            while (i != len)
+            {
+                pt = m[i];
+                pt = pt - 96;
+                k = 1;
+                for (j = 0; j < key; j++)
+                {
+                    k = k * pt;
+                    k = k % n;
+                }
+                temp[i] = k;
+                ct = k + 96;
+                en[i] = ct;
+                i++;
+            }
+            en[i] = -1;
+
+            for (i = 0; i< en.Length; i++)
+            {
+                if (en[i] != -1)
+                {
+                    textBox1.Text += (char)en[i]; //Convert.ToChar(Convert.ToInt16(en[i]));//(Convert.ToString(Convert.ToChar((en[i]))));
+                }
+            }
+        }
+
+        void decrypt()
+        {
+             int pt, ct, key = d[0], k;
+             i = 0;
+            while (en[i] != -1)
+            {
+                ct = temp[i];
+                k = 1;
+             for (j = 0; j < key; j++)
+             {
+                k = k * ct;
+                k = k % n;
+             }
+               pt = k + 96;
+               m[i] = pt;
+                i++;
+            }
+                m[i] = -1;
+
+            for (i = 0; m[i] != -1; i++)
+            {
+                //textBox2.Text = (" " + m[i]);
+                textBox2.Text += Convert.ToChar(Convert.ToInt16(m[i]));  //(Convert.ToString(Convert.ToChar((m[i]))));
+                
+            }
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = false;
+            textBox3.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+
+
+            Random r = new Random();
+             p = r.Next(1, 50);
+
+            
+            while (!ePrimo(p))
+            {
+                p = r.Next(1, 50);
+            }
+
+            Random r2 = new Random();
+            q = r.Next(1, 50);
+
+
+            while (!ePrimo(q))
+            {
+                q = r.Next(1, 50);
+            }
+
+            
+
+            n = p * q;
+            t = (p - 1) * (q - 1);
+
+            
+
+            
+            msg = textBox3.Text.ToCharArray();
+
+
+
+            for (i = 0; i < msg.Length; i++) 
+            {
+                m[i]= msg[i];
+                
+            }
+            ce();
+            encriptar();
+
+            label4.Text = "P: " + p;
+            label5.Text = "q: " + q;
+            label6.Text = "n: " + n;
+           
+
+            
+            
+        }
+    }
+}
